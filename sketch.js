4a678dbf58cf5bffa5e2e27c4411fc0c40aab354
Pratinavagarwala <66483787+Pratinavagarwala@users.noreply.github.com>
2020-07-17 21:06:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//Create variables here
+var happydog,database,foodS,foodStack;
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(500,500);
+  var dog;
+}
+
+
+function draw() {  
+  background((46, 139, 8)
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogHappy);
+  }
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
